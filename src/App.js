48ce32d50fdb77c903f32529dd02a94d5da1d449
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-08-18 03:34:10 +0200

    creating the layout container

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Layout from '/Containers/Layout/Layout';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render(){
+    return (
+      <Layout>
+        <Switch>
+          <Route path="/" component={}/>
+          <Route path="/#/NewGame" component={}/>
+          <Route path="/#/ScoreBoard" component={}/>
+        </Switch>
+      </Layout>
+    );
+  }
 }
 
 export default App;
