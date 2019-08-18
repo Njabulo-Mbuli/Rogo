a8a33305bc8cb71f6bd5b6f7019320a78e66f9b8
Njabulo Mbuli <admin@njabulombuli.co.za>
2019-08-18 13:58:37 +0200

    Added the Particle background

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Toolbar from '../../Components/Toolbar/Toolbar';
 import SideDrawer from '../../Components/SideDrawer/SideDrawer';
+import Particles from 'react-particles-js';
 
 
 class Layout extends React.Component{
@@ -19,14 +20,75 @@ class Layout extends React.Component{
 
 	render(){
 		return(
-			<React.Fragment>
+			<div style={{height:"100vh",backgroundColor:"white"}}>
 				<Toolbar toggleSideDrawer={()=>{
 					this.showSideDrawerToggle()
 				}}/>
 				<SideDrawer show={this.state.showSideDrawer}
 							toggleView={this.showSideDrawerToggle}/>
+				<Particles 
+                style={{position:"absolute",top:"0",left:"0",width:"100vw",height:"100vh"}}
+                params={{
+                "particles": {
+                    "number": {
+                        "value": 20,
+                        "density": {
+                            "enable": true,
+                            "value_area": 1803.4120608655228
+                        }
+                    },
+                    "color": {
+                        "value": "#EE6363"
+                    },
+                    "shape": {
+                        "type": "circle",
+                        "polygon": {
+                            "nb_sides": 4
+                        },
+                        "image": {
+                            "src": "img/github.svg",
+                            "width": 100,
+                            "height": 100
+                        }
+                    },
+                    "opacity": {
+                        "value": 0.4,
+                        "random": false,
+                        "anim": {
+                            "enable": false,
+                            "speed": 1,
+                            "opacity_min": 0.2,
+                            "sync": false
+                        }
+                    },
+                    "size": {
+                        "value": 80,
+                        "random": true,
+                        "anim": {
+                            "enable": false,
+                            "speed": 30,
+                            "size_min": 60,
+                            "sync": false
+                        }
+                    },
+                    "move": {
+                        "enable": true,
+                        "speed": 6,
+                        "direction": "none",
+                        "random": false,
+                        "straight": false,
+                        "out_mode": "out",
+                        "bounce": false,
+                        "attract": {
+                            "enable": false,
+                            "rotateX": 600,
+                            "rotateY": 1200
+                        }
+                    }
+                }
+            }} />
 				{this.props.children}
-			</React.Fragment>
+			</div>
 		);
 	}
 }
